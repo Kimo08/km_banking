40ffdbbb4e80b48cee3ab0e905426ea95c924f75
Kimo <kennedy.obasi@prunedge.com>
2024-09-09 16:15:10 +0100

    implement complete layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,41 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
+
+const Home = () => {
+  const loggedIn = {
+    firstName: "Kimo",
+    lastName: "Zabi",
+    email: "contact@zabi.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250000.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250000 }, { currentBalance: 12500 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
